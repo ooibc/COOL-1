ae50a10226e68edc75ae9b6389e49dae5c29c915
NLGithubWP <xingnaili14@gmail.com>
2022-04-24 23:15:03 +0800

    Add Utils

---
+++ cool-queryserver/src/main/java/com/nus/cool/queryserver/utils/Util.java
@@ -0,0 +1,12 @@
+package com.nus.cool.queryserver.utils;
+
+import java.util.Date;
+
+public class Util {
+
+    public static void getTimeClock(){
+        System.out.println("======================== " + new Date() + " ========================");
+    }
+
+
+}
